688f7ac239492656851591cda2df9adbde3330be
Selim <selimgomri@gmail.com>
2022-03-02 09:20:09 +0100

    Revert "installed necessary packages"
    This reverts commit b7754d079d87b73458980d7c88c2cead23b75d95.

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
